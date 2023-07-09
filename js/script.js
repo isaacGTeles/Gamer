4fda6984bf413da17d2133eba2178c166c79f9d7
Isaac Gabriel <isaac.gabriel1999@gmail.com>
2023-07-08 23:27:22 -0300

    gamer-over

--- js/script.js
+++ js/script.js
@@ -1,11 +1,35 @@
 const kratos = document.querySelector('.kratos');
+const pipe = document.querySelector('.pipe');
 
 const jump = () => {
     kratos.classList.add('jump');
 
     setTimeout(() => {
         kratos.classList.remove('jump');
-    }, 500)
+    }, 500);
 }
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const kratosPosition = +window.getComputedStyle(kratos).bottom.replace('px', '');
+
+    console.log(kratosPosition);
+    
+    if (pipePosition <= 90 && pipePosition > 0 && kratosPosition < 70) {
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        kratos.style.animation = 'none';
+        kratos.style.bottom  = `${kratosPosition}px`;
+
+        kratos.src = './images/kra.gif';
+        kratos.style.width = '85px'
+        kratos.style.marginLeft = '50px'
+
+    }
+
+}, 10);
 
 document.addEventListener('keydown', jump);
